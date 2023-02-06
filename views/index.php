9850b1457c299bec9f87ad75b4659ecdb0db2136
diegopinzon07 <diegosebastianpinzongonzalez7@gmail.com>
2023-02-05 22:43:55 -0500

    mi app

---
+++ views/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>maqueta 1</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+
+<body>
+    <div class=".container-fluid tamaño">
+        <br>
+        <div class="centrar">
+            <h5 class="letra1">Aprende Crochet y crea tu negocio</h5>
+        </div>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item video" src="https://www.youtube.com/embed/l9cFvtXH5Bo" title="CLASE 1: Nudo Deslizado y Cadenas (SLIPKNOT AND CHAINS) - Curso Básico de Crochet para Principiantes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+        </div>
+        <div class="card-group">
+            <div class="card">
+
+                <div class="card-group">
+                    <div class="card">
+                        <div class="card-body fondo1">
+                            <h5 class="card-title blanca">Aprende a crear tus propias bolsas, carteras y accesorios sin salir de casa. Deja volar tu imaginación y empieza a convertir tus ideas en piezas únicas, exclusivas y lo mejor todo, hechas a mano a por ti.</h5>
+                            <h5 class="blanca">Este curso es para ti si:</h5>
+                            <div class="card-group">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <p class="card-text">
+                                        <ol>
+                                            <li>
+                                                ¿Quieres empezar a tejer con crochet y no sabes por dónde empezar?
+                                            </li>
+                                            <li>
+                                                ¿Crees que aprender crochet es muy costoso, lleva mucho tiempo y es difícil?
+
+                                            </li>
+                                            <li>
+                                                ¿Siempre has querido aprender a tejer tus propias piezas, pero nunca te has animado?
+                                            </li>
+                                            <li>
+                                                ¿Te apasiona el crochet y todavía no &nbsp&nbsp&nbspsabes cómo generar ingresos con él?
+                                            </li>
+                                            <li>
+                                                ¿Tienes interés por empezar un nuevo pasatiempo que te ayude a relajarte,disminuir tus niveles de estrés y &nbspemprender en el futuro?
+
+                                            </li>
+                                            <li>
+                                                ¿Quieres emprender en algo que realmente te guste y apasione?
+                                            </li>
+                                        </ol>
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <br>
+                            <h5 class="blanca">¿Qué aprenderás en este curso?</h5>
+                            <div class="card-group">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <p class="card-text">
+                                        <ol>
+                                            <li>
+                                                Aprenderás a tejer 4 carteras en formato video y 5 carteras en formato ebook.
+                                            </li>
+                                            <li>
+                                                Conocer todas las herramientas e implementos que necesitas para empezar a tejer.
+
+                                            </li>
+                                            <li>
+                                                Realizar desde cero distintos tipos de puntos para empezar el tejido de tus piezas.
+                                            </li>
+                                            <li>
+                                                Descubrir todos los consejos, tips y trucos para confeccionar piezas de la más alta calidad.
+                                            </li>
+                                            <li>
+                                                Tejer punto por punto carteras de diferentes formas, tamaños y colores totalmente personalizadas por ti.
+
+                                            </li>
+                                        </ol>
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <br>
+                            <h5 class="blanca">Descripción</h5>
+                            <div class="card-group">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <p class="card-text">
+                                        
+                                            <ul>
+                                                <p>En este curso encontrarás todas las bases para empezar tejer todas tus ideas y convertirlas en las más exclusivas bolsas y carteras para sorprender a tus seres queridos o incluso iniciar tu propio emprendimiento de alta rentabilidad. Lo mejor de todo, no necesitas tener conocimientos previos, aquí aprenderás desde cero.</p>
+                                                <p>Bajo las instrucciones de Leydy Guerrero, profesional en el área de la Administración de empresas y apasionada por el crochet desde hace más de 20 años; empezarás puntada tras puntada a materializar todas tus ideas y convertirlas en piezas terminadas. Aprenderás además, a conocer desde cero todas las herramientas, técnicas y tips que necesitas para empezar a tejer tus propias bolsas y carteras de la más alta calidad.</p>
+                                                <p>Al finalizar este curso estarás listo para empezar a sorprender con tus habilidades y empezar tu propio negocio en el mundo del crochet.</p>
+                                        
+                                            <ul>
+                                        
+                                            
+                
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <br>
+                            <h5 class="blanca">Temario del curso</h5>
+                            <div class="card-group">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <p class="card-text">
+                                        
+                                            <ul>
+                                                <p>En este curso encontrarás todas las bases para empezar tejer todas tus ideas y convertirlas en las más exclusivas bolsas y carteras para sorprender a tus seres queridos o incluso iniciar tu propio emprendimiento de alta rentabilidad. Lo mejor de todo, no necesitas tener conocimientos previos, aquí aprenderás desde cero.</p>
+                                                <p>Bajo las instrucciones de Leydy Guerrero, profesional en el área de la Administración de empresas y apasionada por el crochet desde hace más de 20 años; empezarás puntada tras puntada a materializar todas tus ideas y convertirlas en piezas terminadas. Aprenderás además, a conocer desde cero todas las herramientas, técnicas y tips que necesitas para empezar a tejer tus propias bolsas y carteras de la más alta calidad.</p>
+                                                <p>Al finalizar este curso estarás listo para empezar a sorprender con tus habilidades y empezar tu propio negocio en el mundo del crochet.</p>
+                                        
+                                            <ul>
+                                        
+                                            
+                
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+
+
+
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body fondo1">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+
+
+    </div>
+
+    <script src="../assets/js/jquery-3.6.3.min.js"></script>
+    <script src="../assets/js/bootstrap.js"> </script>
+
+</body>
+
+</html>
